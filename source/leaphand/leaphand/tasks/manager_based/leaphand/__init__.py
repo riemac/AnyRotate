4dc2890728aa57a7e3ea32faf0ce0ec6fe22b3d5
riemac <18817066197@163.com>
2025-10-26 21:07:10 +0800

    delete agents and adjust task register

--- source/leaphand/leaphand/tasks/manager_based/leaphand/__init__.py
+++ source/leaphand/leaphand/tasks/manager_based/leaphand/__init__.py
@@ -8,51 +8,18 @@
 import gymnasium as gym
 
 from . import agents
-from .leaphand_continuous_rot_env_cfg import LeaphandContinuousRotEnvCfg
-from .inhand_env_cfg import InHandEnvCfg
-from .inhand_env_v1_cfg import InHandEnvV1Cfg
-from .inhand_env_v2_cfg import InHandEnvV2Cfg
+from .inhand_base_env_cfg import InHandObjectEnvCfg
 
 ##
 # Register Gym environments.
 ##
 
 gym.register(
-    id="Template-Leaphand-ContinuousRot-Manager-v0", # Template可被list_envs.py识别（但不影响环境注册与训练）
+    id="Template-Leaphand-Rot-Manager-v0", # Template可被list_envs.py识别（但不影响环境注册与训练）
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.leaphand_continuous_rot_env_cfg:LeaphandContinuousRotEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.inhand_base_env_cfg:InHandObjectEnvCfg",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
     },
-)
-
-gym.register(
-    id="Template-InHand-Manager-v0", # [项目名]-[任务名]-[架构名]-v[版本号]
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.inhand_env_cfg:InHandEnvCfg", # 包路径.模块名:类名
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_inhand_cfg.yaml",
-    },
-)
-
-gym.register(
-    id="Template-InHand-Manager-v1", # [项目名]-[任务名]-[架构名]-v[版本号]
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.inhand_env_v1_cfg:InHandEnvV1Cfg", # 包路径.模块名:类名
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_inhand_v1_cfg.yaml",
-    },
-)
-
-gym.register(
-    id="Template-InHand-Manager-v2", # [项目名]-[任务名]-[架构名]-v[版本号]
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.inhand_env_v2_cfg:InHandEnvV2Cfg", # 包路径.模块名:类名
-        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_inhand_v2_cfg.yaml",
-    }
 )
